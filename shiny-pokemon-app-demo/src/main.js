6d6f7c7fe14c76acec9b260a8cecacb181a7ac16
Susy <ssusannamn@gmail.com>
2025-08-17 12:37:41 +0300

    fixed adding a new hunt and continue hunt
    - new hunt creates a card in continue the hunt section
    - saves current hunt to local storage, key currentH
    - pushes the new hunt to TargetPkmn array and saves array to local storage
    - continue hunt counter works now, saves the new count to ls as currentH
    - fixed display errors after refreshing page

--- shiny-pokemon-app-demo/src/main.js
+++ shiny-pokemon-app-demo/src/main.js
@@ -1,6 +1,4 @@
 // npx @tailwindcss/cli -i ./src/input.css -o ./src/output.css --watch
-
-// INSTEAD OF CLOSING EVERY DIV ALWAYS, JUST RELOAD THE PAGE WHEN U CLOSE A SECTION
 function checkList(key) {
   if (JSON.parse(localStorage.getItem(key)) != null)
     return JSON.parse(localStorage.getItem(key));
@@ -25,71 +23,21 @@ const headerTxt = ["continue hunt", "new hunt", "target list", "caught shinies",
 
 let targetPkmn = checkList("targetList");
 let caughtPkmn = [];
+let currentPkmn = null;
 
 // call on load if there is a current hunt id
 function checkCurrentHunt() {
-  if (JSON.parse(localStorage.getItem("currentHuntID")) != null) {
-    let index = localStorage.getItem("currentHuntID");
-    document.getElementById("idcard").innerHTML = targetPkmn[index].id;
-    document.getElementById("pkmcard").innerHTML = targetPkmn[index].pokemon;
-    document.getElementById("methodcard").innerHTML = targetPkmn[index].method;
-    document.getElementById("continuenav").classList.remove("hidden");
+  if (JSON.parse(localStorage.getItem("currentH")) != null) {
+    let currentH = JSON.parse(localStorage.getItem("currentH"));
+    document.getElementById("idcard").innerHTML = currentH.id;
+    document.getElementById("pkmcard").innerHTML = currentH.pokemon;
+    document.getElementById("methodcard").innerHTML = currentH.method;
+    document.getElementById("datecard").innerHTML = currentH.date;
+    document.getElementById("countcard").innerHTML = currentH.count;
+    document.getElementById("continuenav").classList.toggle("hidden");
   }
-}
-
-// check if localstorage has something in it
-
-// class Target {
-//   constructor(name, method, count) {
-//     this._name = name;
-//     this._method = method;
-//     this._encounters = count;
-//   }
-//   get name() {
-//     return this._name;
-//   }
-//   set name(x) {
-//     this._name = x;
-//   }
-//   get method() {
-//     return this._method;
-//   }
-//   set method(x) {
-//     this._method = x;
-//   }
-//   get count() {
-//     return this._encounters;
-//   }
-//   set count(x) {
-//     this._encounters = x;
-//   }
-// }
-
-// class Pokemon extends Target {
-
-// }
-
-// debug function
-// section name: string
-// function showSection(sectionName) {
-//   const element = document.getElementById(sectionName);
-//   element.classList.toggle("hidden");
-// }
-
-// function testTarget() {
-//   let test = new Target("asd", "zzz", 0);
-
-//   console.log(test.name);
-//   console.log(test.method);
-//   console.log(test.count);
-
-//   test = null;
-// }
-
-// for testing
-function clearLS() {
-  localStorage.clear();
-  console.log("cleared localstorage");
+  else
+    console.log("data not found");
 }
 
 function toIndex() {
@@ -104,12 +52,6 @@ function toggleSelections(arrayID) {
   document.getElementById("header-text").innerHTML = headerTxt[arrayID];
 }
 
-// not working, find out why
-function toggleMultiple(selections) {
-  for (let i = 0; i < selections.length; i++)
-    document.getElementById(selections[i]).classList.toggle("hidden");
-}
-
 // manage mobile nav bar visibility
 function showNavbar() {
   document.getElementById("close1").classList.toggle("hidden");
@@ -117,6 +59,13 @@ function showNavbar() {
   document.getElementById("header-text").innerHTML = headerTxt[headerTxt.length - 1];
   document.getElementById("nav").classList.toggle("hidden");
   document.getElementById("tarl").innerHTML = ""; // clear target list
+
+  if (!document.getElementById("continueh").classList.contains("hidden")) {
+    let currentH = JSON.parse(localStorage.getItem("currentH"));
+    targetPkmn[currentH.id].count = currentH.count;
+    localStorage.setItem("targetList", JSON.stringify(targetPkmn));
+    console.log("updated target list LS");
+  }
 
   // if an element is visible, hide it by toggling hidden class
   for (let i = 0; i < selections.length; i++) {
@@ -127,31 +76,31 @@ function showNavbar() {
 
 // add/substract to encounter count
 function changeCount(symbol) {
-  let num = Number(document.getElementById("countcard").innerHTML);
-  let id = JSON.parse(localStorage.getItem("currentHuntID"));
-  if (symbol == '+') {
-    document.getElementById("countcard").innerHTML = String(num + 1);
-    targetPkmn[id].count = num;
+  let currentH = JSON.parse(localStorage.getItem("currentH"));
+  let count = Number(document.getElementById("countcard").innerHTML);
+
+  if (symbol == "-") {
+    count -= 1;
   }
-  else if (symbol == '-') {
-    document.getElementById("countcard").innerHTML = String(num - 1);
-    targetPkmn[id].count = num;
+  else if (symbol == "+") {
+    count += 1;
   }
-  localStorage.setItem("targetList", JSON.stringify(targetPkmn));
-  console.log(targetPkmn[id].count);
+
+  document.getElementById("countcard").innerHTML = String(count);
+  currentH.count = count;
+  localStorage.setItem("currentH", JSON.stringify(currentH));
 }
 
 // fetch data from new hunt form, return an array that has
-// id, pokemon, method and date
 function getFormData() {
-  // let data = [];
   let id = targetPkmn.length;
   let pokemon = document.getElementById("pkm").value;
   let method = document.getElementById("method").value;
   let date = document.getElementById("date").value;
   let data = new Target(id, 0, pokemon, method, date, 0);
 
-  localStorage.setItem("currentHuntID", JSON.stringify(data.id));
+  // save target to ls as currentH
+  localStorage.setItem("currentH", JSON.stringify(data));
   return data;
 }
 
@@ -162,23 +111,20 @@ function createHuntCard() {
   document.getElementById("idcard").innerHTML = target.id;
   document.getElementById("pkmcard").innerHTML = target.pokemon;
   document.getElementById("methodcard").innerHTML = target.method;
+  document.getElementById("datecard").innerHTML = target.date;
   document.getElementById("countcard").innerHTML = target.count;
 
   targetPkmn.push(target);
+  localStorage.setItem("currentH", JSON.stringify(target));
   localStorage.setItem("targetList", JSON.stringify(targetPkmn));
 }
 
 // close new hunt, open continue hunt, reset form
-// save target to array, save array to localstorage
 function showHuntCard() {
   createHuntCard();
-
-  // change to ([]) ??
-  //toggleMultiple("newh", "continueh", "continuenav");
   document.getElementById("newh").classList.toggle("hidden");
   document.getElementById("continueh").classList.toggle("hidden");
-  if (JSON.parse(localStorage.getItem("currentHuntID")) != null)
-    document.getElementById("continuenav").classList.remove("hidden");
+  document.getElementById("continuenav").classList.remove("hidden");
   document.getElementById("pkmnform").reset();
 }
 
@@ -200,10 +146,21 @@ function showTargetList() {
   toggleSelections(2);
 }
 
+// for testing--------------------------------------------------
+function clearLS() {
+  localStorage.clear();
+  console.log("cleared localstorage");
+}
 // debug func
 function showTargetsInLS() {
   if (JSON.parse(localStorage.getItem("targetList")) != null)
     console.log(JSON.parse(localStorage.getItem("targetList")));
+  else
+    console.log("nothing");
+}
+function showCurrentHuntInLS() {
+  if (JSON.parse(localStorage.getItem("currentH")) != null)
+    console.log(JSON.parse(localStorage.getItem("currentH")));
   else
     console.log("nothing");
 }
