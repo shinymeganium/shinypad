cd90d13e574620df35bfc821ec93ef6f19c413f8
Susy <ssusannamn@gmail.com>
2025-08-20 22:22:53 +0300

    fixing layout
    getting main elements to work visually

--- shiny-pokemon-app-demo/src/main.js
+++ shiny-pokemon-app-demo/src/main.js
@@ -55,10 +55,18 @@ let currentHunt = getSavedData("currentHunt", false);
 //   window.scrollTo(0,0);
 // }
 
-function toggleSel(sID, navID = "nav-mobile") {
+function toggleSelection(sID, navID = "navmobile") {
+  document.getElementById("title").classList.toggle("hidden");
   document.getElementById(sID).classList.toggle("hidden");
+  document.getElementById("maincontent").classList.toggle("bg-susylightgray");
   if (navID != "")
     document.getElementById(navID).classList.toggle("hidden");
+}
+
+function toggleBig(sID) {
+  document.getElementById("maincontent").classList.toggle("xl:hidden");
+  document.getElementById(sID).classList.toggle("hidden");
+  console.log("yup");
 }
 
 function createNewTarget() {
@@ -192,6 +200,52 @@ function showChangeHunt(listID, sID, headerTxt, changeBtn) {
     document.getElementById("s-current-hunt").classList.toggle("hidden");
   }
   toggleSelection(sID, headerTxt, changeBtn);
+}
+
+// -----------------------------------------------------
+
+// Make the DIV element draggable:
+dragElement(document.getElementById("maincontent"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
 }
 
 // ---------------------------------- DEBUG FUNCTIONS ----------------------------------
